1b73372a5f0469c91b79f18900a8b09e3902b510
anna5677 <97455649+anna5677@users.noreply.github.com>
2022-01-10 15:56:23 +0100

    Add files via upload

---
+++ Passwort.py
@@ -0,0 +1,22 @@
+
+# In diesem Program wird nach einem Passwort gefragt, welches man korrekt eingeben muss. 
+
+# Es erscheit ein Feld, in dem man das Passwort eingeben kann.
+x = input("Login eingeben: ")
+
+# In diesem Programm ist das korrekte Passwort "Strudel123". Dies kann man aber nach belieben ändern.
+if x == "Strudel123":
+    # Ist das eingegebene Passwort richtig, erscheint "Herzlich Willkommen". 
+    print("Herzlich Willkommen")
+    # Ist das eingegebene Passwort jedoch falsch, erscheint "Passwort nicht korrekt".
+else: 
+        print("Passwort nicht korrekt")
+
+
+
+
+
+
+
+
+
