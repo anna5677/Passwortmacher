e75e0b99b4052821a2cfcd0cab67ebea2368fbec
anna5677 <97455649+anna5677@users.noreply.github.com>
2022-01-10 16:12:44 +0100

    Update Passwort.py

--- Passwort.py
+++ Passwort.py
@@ -1,3 +1,14 @@
+**Passwortmacher**
+# Einleitung: In diesem Program können Sie Ihr Passwort eingeben und es überprüfen für Sie später, ob es richtig ist. Wenn Sie das korrekte Passwort eingegeben haben, bergrüsst es Sie mit "Herzlich Willkommen". Ist es jedoch das falsche Passwort, gibt das Program als Rückmeldung: "Passwort nicht korrekt". Sie können das Passwort aber natürlich nach Belieben verändern.
+
+# Features: 
+
+
+
+# Reflexion: Ich habe gelernt, dass die Programmiersprachen sehr viel komlexer sind, als ich mir vorgestellt habe. Ich habe gelernt, dass man praktisch alles programmieren kann. Über Einkaufslisten zu Uhrzeitenberechner bis hin zu einem Programm, das den Notendurchschnitt ausrechnen kann.
+
+
+
 
 # In diesem Program wird nach einem Passwort gefragt, welches man korrekt eingeben muss. 
 
@@ -12,11 +23,15 @@ if x == "Strudel123":
 else: 
         print("Passwort nicht korrekt")
 
-
-
-
-
-
-
-
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
